e40ffa8e4e50e66c6844a6eec8212c7348949469
frombunny <rucy011121@gamil.com>
2024-07-16 05:07:53 +0700

    :rocket: CI. 테스트

--- src/test/java/com/likelion/RePlay/RePlayApplicationTests.java
+++ src/test/java/com/likelion/RePlay/RePlayApplicationTests.java
@@ -3,11 +3,19 @@ package com.likelion.RePlay;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest
-class RePlayApplicationTests {
+public class RePlayApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// 애플리케이션 컨텍스트가 성공적으로 로드되는지
 	}
 
+	@Test
+	void testPrintName() {
+		// printName 메소드가 예외를 던지지 않고 실행되는지
+		assertDoesNotThrow(() -> RePlayApplication.printName("RePlay"));
+	}
 }

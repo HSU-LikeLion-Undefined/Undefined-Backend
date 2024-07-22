51b811e8ae141967c1590351e0cd7ab22efb9ccb
frombunny <rucy011121@gamil.com>
2024-07-22 17:11:48 +0900

    :lock: ADD. AdminController 생성

---
+++ src/main/java/com/likelion/RePlay/domain/user/web/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.likelion.RePlay.domain.user.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "admin";
+    }
+}

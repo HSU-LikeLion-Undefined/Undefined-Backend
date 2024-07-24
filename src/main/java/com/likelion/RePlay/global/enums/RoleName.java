522a39c4c8553614681fccf58f2b7b022803e6f5
frombunny <rucy011121@gamil.com>
2024-07-25 00:42:18 +0900

    :lock: FEAT. JwtTokenProvider 작성

--- src/main/java/com/likelion/RePlay/global/enums/RoleName.java
+++ src/main/java/com/likelion/RePlay/global/enums/RoleName.java
@@ -1,5 +1,12 @@
 package com.likelion.RePlay.global.enums;
 
-public enum RoleName {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum RoleName implements GrantedAuthority {
     ROLE_ADMIN, ROLE_CLIENT;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }

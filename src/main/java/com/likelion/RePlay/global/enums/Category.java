9e3f513e9a2d08855b964da14a2f4125fbfc314f
ujii <wcm9763@naver.com>
2024-07-26 13:34:29 +0900

    :ambulance: FIX. Category 주석 위치로 인한 DB 데이터 타입 오류 수정

--- src/main/java/com/likelion/RePlay/global/enums/Category.java
+++ src/main/java/com/likelion/RePlay/global/enums/Category.java
@@ -1,5 +1,16 @@
 package com.likelion.RePlay.global.enums;
 
 public enum Category {
-    IMPROVEMENT // 자기계발, LANGUAGE, SNS, BOOK, CULTURE, PICNIC // 나들이, TRIP, EXERCISE, RESTAURANT, ENTERTAINMENT, COOKING, ETC
+    IMPROVEMENT, // 자기계발
+    LANGUAGE,
+    SNS,
+    BOOK,
+    CULTURE,
+    PICNIC, // 나들이
+    TRIP,
+    EXERCISE,
+    RESTAURANT,
+    ENTERTAINMENT,
+    COOKING,
+    ETC
 }

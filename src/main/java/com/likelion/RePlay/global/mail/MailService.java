652c6506e6410b1d7e9432b1d7f0149fb4858ad8
frombunny <rucy011121@gamil.com>
2024-07-28 01:48:58 +0900

    :sparkles: FEAT. InfoSubmitDto 작성

--- src/main/java/com/likelion/RePlay/global/mail/MailService.java
+++ src/main/java/com/likelion/RePlay/global/mail/MailService.java
@@ -1,20 +1,36 @@
 package com.likelion.RePlay.global.mail;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @Service
 public class MailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
-    public void sendSimpleMail(String subject, String content) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setSubject(subject);
-        message.setText(content);
-        message.setTo("lucy01112@naver.com");
+    public void sendMail(String subject, String content, List<MultipartFile> attachments)
+            throws MessagingException, IOException {
+        // MimeMessage의 경우 멀티파트 데이터를 처리 할 수 있고
+        // SimpleMailMessage는 단순한 텍스트 데이터만 전송이 가능
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setSubject(subject);
+        helper.setText(content);
+        helper.setTo("lucy01112@naver.com");
+
+        // 첨부파일 추가
+        for (MultipartFile file : attachments) {
+            helper.addAttachment(file.getOriginalFilename(), file);
+        }
 
         javaMailSender.send(message);
     }

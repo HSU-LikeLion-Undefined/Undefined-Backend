0163fb5b6417ffab2cbe8672f0ec0b2b6e9e5f56
ujii <wcm9763@naver.com>
2024-07-27 21:38:15 +0900

    :package: ADD. Scrap Repository 작성

---
+++ src/main/java/com/likelion/RePlay/domain/playing/repository/PlayingScrapRepository.java
@@ -0,0 +1,10 @@
+package com.likelion.RePlay.domain.playing.repository;
+
+import com.likelion.RePlay.domain.playing.entity.PlayingScrap;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PlayingScrapRepository extends JpaRepository<PlayingScrap, Long> {
+    Optional<PlayingScrap> findByUserPhoneId(String phoneId);
+}

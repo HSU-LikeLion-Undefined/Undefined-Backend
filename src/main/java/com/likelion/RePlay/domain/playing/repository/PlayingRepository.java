2477f568050b07a11f362d12839b9b6d8fe1ae63
ujii <wcm9763@naver.com>
2024-07-28 15:13:24 +0900

    :fire: DEL. QueryDSL 삭제

--- src/main/java/com/likelion/RePlay/domain/playing/repository/PlayingRepository.java
+++ src/main/java/com/likelion/RePlay/domain/playing/repository/PlayingRepository.java
@@ -2,7 +2,9 @@ package com.likelion.RePlay.domain.playing.repository;
 
 import com.likelion.RePlay.domain.playing.entity.Playing;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface PlayingRepository extends JpaRepository<Playing, Long>, QuerydslPredicateExecutor<Playing> {
+import java.util.Optional;
+
+public interface PlayingRepository extends JpaRepository<Playing, Long> {
+
 }

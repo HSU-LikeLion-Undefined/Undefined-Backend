4b450520f505ffe1e1d480dd91070865752598dd
ujii <wcm9763@naver.com>
2024-07-29 16:15:33 +0900

    :lock: SECURITY. SecurityConfig playing API 경로 오류 수정

--- src/main/java/com/likelion/RePlay/global/security/SecurityConfig.java
+++ src/main/java/com/likelion/RePlay/global/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                         //비로그인 사용자도 접근 가능
                         .requestMatchers("/api/user/signUp", "/api/user/login",
                                 "/api/user/isExistNickName","/api/user/isExistPhoneId", "/api/user/getMyPage").permitAll()
-                        .requestMatchers("/api/playing/getPlayings/**").permitAll()
+                        .requestMatchers("/api/playing/getPlayings/**", "/api/playing/comment/**").permitAll()
                         .requestMatchers("/api/info/getAllInfo", "/api/info/getOneInfo/**").permitAll()
                         .requestMatchers(("/api/learning/getLearning/**")).permitAll()
                         // 로그인한 사용자만 접근 가능

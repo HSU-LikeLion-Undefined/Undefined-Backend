910ccfcd9b7368d6f4ae5d6b48e966e5f40bfcb0
frombunny <rucy011121@gamil.com>
2024-07-30 03:43:35 +0900

    :sparkles: FEAT. UserReviewRequestDto 작성

---
+++ src/main/java/com/likelion/RePlay/domain/playing/web/dto/UserReviewRequestDto.java
@@ -0,0 +1,13 @@
+package com.likelion.RePlay.domain.playing.web.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserReviewRequestDto {
+    private String userName;
+}
+
